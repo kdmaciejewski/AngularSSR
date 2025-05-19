377487d9b8f64c9e3bfbd29f15f94262f76ca6bb
Krzysztof Maciejewski <94606867+kdmaciejewski@users.noreply.github.com>
2025-05-19 12:59:11 +0200

    updating server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,3 @@
-// Error: server.ts doesn't seem to be Netlify compatible and is not known default. Please replace it with Netlify compatible server.ts.
-
-// It seems like you use "CommonEngine" - for this case your server.ts file should contain following:
-
 import { CommonEngine } from "@angular/ssr/node";
 import { render } from "@netlify/angular-runtime/common-engine";
 
@@ -17,29 +13,3 @@ export async function netlifyCommonEngineHandler(request: Request, context: any)
 
   return await render(commonEngine);
 }
-
-// If you want to use "AppEngine" instead - your server.ts file should contain following:
-
-import { AngularAppEngine, createRequestHandler } from "@angular/ssr";
-import { getContext } from "@netlify/angular-runtime/context";
-
-const angularAppEngine = new AngularAppEngine();
-
-export async function netlifyAppEngineHandler(request: Request): Promise<Response> {
-  const context = getContext();
-
-  // Example API endpoints can be defined here.
-  // Uncomment and define endpoints as necessary.
-  // const pathname = new URL(request.url).pathname;
-  // if (pathname === '/api/hello') {
-  //   return Response.json({ message: 'Hello from the API' });
-  // }
-
-  const result = await angularAppEngine.handle(request, context);
-  return result || new Response("Not found", { status: 404 });
-}
-
-/**
- * The request handler used by the Angular CLI (dev-server and during build).
- */
-export const reqHandler = createRequestHandler(netlifyAppEngineHandler);
